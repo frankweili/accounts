b8af6fd6644174cf70e20b3ae6f22c7c587f0717
frankwei <1183806196@qq.com>
2020-11-03 22:10:50 +0800

    创建money的四个组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+               <label class="notes">
+                   <span class="name">备注</span>
+                   <input type="text" placeholder="在这里输入备注">
+               </label>
+           </div>
+</template>
+
+<script>
+    export default {
+        name:'Notes'
+    }
+</script>
+
+<style lang="scss" scoped>
+.notes{
+   
+    font-size: 14px;
+    padding-left: 16px;
+   
+    background: #f5f5f5;
+     display: flex;
+    align-items: center;
+    >.name{
+        padding-right: 16px;
+    }
+    input{
+        height: 64px;
+        flex-grow: 1;
+        padding-right: 16px;
+        background: transparent;
+        border:none
+    }
+}
+
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,40 @@
+<template>
+   <div>
+               <ul class="types">
+               <li class="selected">支出</li>
+               <li>收入</li>
+           </ul>
+           </div>
+</template>
+
+<script>
+    export default {
+        name:'Types'
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.types{
+    background: #c4c4c4;
+    font-size: 24px;
+    display: flex;
+    text-align: center;
+    li{height: 64px;
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;;
+        &.selected::after{
+            content: '';
+            position:absolute;
+            bottom: 0;
+            left: 0;
+width: 100%;
+height: 4px;
+background:#333
+        };
+    }
+}
+</style>
