e5bf5a9661608dea897424d73d802d66a55b0801
frankwei <1183806196@qq.com>
2020-11-04 19:37:23 +0800

    完成Tags的ts部分

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,56 +1,86 @@
 <template>
-         <div  class="tags">
-               
-           <div class="new">
-               <button>新增标签</button>
-           </div>
-           <ul class="current">
-               <li>衣</li>
-               <li>食</li>
-               <li>住</li>
-               <li>行</li>
-            
-           </ul>
-           </div>
+  <div class="tags">
+    <div class="new">
+      <button @click="create">新增标签</button>
+    </div>
+    <ul class="current">
+      <li
+        v-for="tag in dataSource"
+        :key="tag"
+        :class="{ selceted: selectedTags.indexOf(tag) >= 0 }"
+        @click="toggle(tag)"
+      >
+        <!-- 绑定class：selected-->
+        {{ tag }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script>
-    export default {
-        name:'Tags'
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue {
+  @Prop() dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
     }
+  }
+  create() {
+    const name = window.prompt("请输入");
+    if (name === "") {
+      window.alert("请重新输入");
+      window.prompt("请输入");
+    } else if (this.dataSource) {
+      this.$emit("update:dataSource", [...this.dataSource, name]); //出发一个事件，因为无法改父元素上的数据
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.tags{
-    
-    font-size: 14px;
-    padding: 16px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column-reverse;
-    >.current{
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
     display: flex;
     flex-wrap: wrap;
-        >li{
-      background:#d9d9d9;
-    $h:24px;
-    height: $h;
+    > li {
+      $bg: #d9d9d9;
+      background: $bg;
+      $h: 24px;
+      height: $h;
       line-height: $h;
-    border-radius:$h/2;
-  padding: 0 16px;
-     margin-right: 12px;
-        }
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      &.selceted {
+        background: darken($bg, 50%);
+        color: white;
+      }
     }
-    >.new{
-        padding-top: 16px;
-        
-        button{
-            background: transparent;
-           border: none;
-           color: #999;
-           border-bottom: 1px solid;
-           padding:0 4px;
-        }
+  }
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
     }
+  }
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,33 +1,34 @@
 <template >
-       <Layout class-prefix="layout">
-       <NumberPad />
-           <Types/>
-           <Notes/>
-     <Tags/>
-       </Layout>
-   
+  <Layout class-prefix="layout">
+    <NumberPad />
+    <Types />
+    <Notes />
+    <Tags :data-source.sync="tags" />
+  </Layout>
 </template>
 
 <style lang="scss" >
-.layout-content{
-    display: flex;
-    flex-direction: column-reverse;
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
 }
 </style>
 <script>
-import NumberPad from '@/components/Money/NumberPad.vue'
-import Types from '@/components/Money/Types.vue'
-import Tags from '@/components/Money/Tags.vue'
-    import Notes from '@/components/Money/Notes.vue'
-    export default {
-        name:'Money',
-        components:{NumberPad,Types,Tags,Notes}
-    }
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Tags from "@/components/Money/Tags.vue";
+import Notes from "@/components/Money/Notes.vue";
+export default {
+  name: "Money",
+  components: { NumberPad, Types, Tags, Notes },
+  data() {
+    return {
+      tags: ["衣", "食", "住", "行"],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-
-
-
 </style>
