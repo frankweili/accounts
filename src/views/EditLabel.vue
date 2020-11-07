1dc8f0d1497594f66077bba61be4921cb6954242
frankwei <1183806196@qq.com>
2020-11-07 23:36:22 +0800

    创建EdiitLabel.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -2,8 +2,25 @@
   <Layout> 标签页面 </Layout>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import tagListModel from "@/tagListModel";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id; // 获取地址栏中的id
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
